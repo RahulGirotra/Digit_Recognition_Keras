245f1945ea0e9a230bb4b51ef8a9b9c1ce29e598
Rahul <43619829+RahulGirotra@users.noreply.github.com>
2021-10-14 19:12:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -34,27 +34,13 @@ def prediction():
     img=request.files['img']
     img.save("img.jpg")
     image=cv2.imread("img.jpg")
-    print("x",image.shape)
-    #image=cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    print("y",image.shape)
     image = cv2.resize(image, (28,28))
-    print("z",image.shape)
-
-    #image = np.reshape(image, (28,28,1))
     image=image.reshape(1, 28, 28, 1)
     image=image/255
     print(image)
     pred= model.predict(image)
     pred = np.argmax(pred)
-
-    """
-    to_test = X_test[0]
-    to_test = to_test.reshape(1, 28, 28, 1)
-    predict_x0 = model.predict(to_test)
-    classes_x0 = np.argmax(predict_x0, axis=1)
-    classes_x0[0]
-    """
 
     return render_template("prediction.html", data=pred)
 
